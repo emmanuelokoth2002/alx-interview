7cfcfe4829784b0572a93b273c1323896c9dc976
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-11-02 05:36:21 -0700

    Updates

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -6,9 +6,11 @@ import sys
 
 def is_safe(board, row, col, N):
     for i in range(row):
-        if board[i] == col or board[i] - i == col - row
-        or board[i] + i == col + row:
-            return False
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+           return False
+
     return True
 
 
