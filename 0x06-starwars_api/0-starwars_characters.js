efca3a4024ba5222b17a3d2707c5dcb1d286434a
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-11-12 03:59:23 -0800

    Updates

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieID = process.argv[2];
+const movieId = process.argv[2];
+const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-if (!movieID) {
-  console.error('Please provide a Movie ID.');
-  process.exit(1);
-}
-
-const url = `https://swapi.dev/api/films/${movieID}/`;
-
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const filmData = JSON.parse(body);
-    const characters = filmData.characters;
-
-    characters.forEach((characterURL) => {
-      request(characterURL, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        } else {
-          console.error('Failed to fetch character:', error);
-        }
-      });
-    });
-  } else {
-    console.error('Failed to fetch movie data:', error);
-  }
+request(movieUrl, (err, res, body) => {
+  if (err) console.log(err);
+  const index = 0;
+  const characters = JSON.parse(body).characters;
+  displayName(characters, index);
 });
+
+const displayName = function (url, index) {
+  request(url[index], (err, res, body) => {
+    if (err) console.log(err);
+    console.log(JSON.parse(body).name);
+    if (++index < url.length) {
+      displayName(url, index++);
+    }
+  });
+};
