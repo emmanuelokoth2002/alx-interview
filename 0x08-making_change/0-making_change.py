0df566b0141c4b5b5636a4b329572af9ff28f5d0
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-11-22 04:36:52 -0800

    Updates

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,15 +7,6 @@ Module for making change using the fewest number of coins
 def makeChange(coins, total):
     """
     Determine the fewest number of coins needed to meet a given total.
-
-    Args:
-        coins (list): A list of the values of the coins in your possession.
-        total (int): The target total amount.
-
-    Returns:
-        int: Fewest number of coins needed to meet the total.
-             If total is 0 or less, return 0.
-             If total cannot be met by any number of coins you have, return -1.
     """
     if total < 1:
         return 0
