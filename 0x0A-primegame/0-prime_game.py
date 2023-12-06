86d2584820651b2b815ba35539a75874458c10ca
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-12-06 21:19:02 +0300

    Updates

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
 
-def is_prime(num):
-    if num < 2:
+def is_prime(n):
+    """
+    This function checks for prime numbers.
+    """
+    if n <= 1:
         return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
             return False
     return True
 
-def isWinner(x, nums):
-    ben_wins = 0
+
+def is_winner(x, nums):
+    """
+    it then determines the winner of the game.
+    """
     maria_wins = 0
-
-    for n in nums:
-        prime_numbers = [i for i in range(2, n + 1) if is_prime(i)]
-        prime_set = set(prime_numbers)
-        
-        moves = 0
-        while prime_set:
-            if moves % 2 == 0:
-                selected = max(prime_set)
-                prime_set -= set(range(selected, n + 1, selected))
-                maria_wins += 1
+    ben_wins = 0
+    for _ in range(x):
+        n = nums.pop()
+        while n > 1:
+            for num in nums:
+                if is_prime(num) and n % num == 0:
+                    n //= num
+                    break
             else:
-                selected = max(prime_set)
-                prime_set -= set(range(selected, n + 1, selected))
-                ben_wins += 1
-            moves += 1
-
-    if ben_wins > maria_wins:
-        return "Ben"
-    elif maria_wins > ben_wins:
+                break
+        if n == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+    if maria_wins > ben_wins:
         return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     else:
         return None
